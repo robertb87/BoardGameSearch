5f204031a8a9e4299c984a13f8df6652d5bc11f5
Robert <55030468+robertb87@users.noreply.github.com>
2023-05-06 11:51:28 +0100

    initial commit

--- Sources/BoardGameSearch/BoardGameSearch.swift
+++ Sources/BoardGameSearch/BoardGameSearch.swift
@@ -1,6 +1,75 @@
+import Foundation
+
 public struct BoardGameSearch {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+}
+
+struct GameDetails: Codable {
+    let id: String
+    let name: String
+    let names: [String]
+    let year_published: Int
+    let min_players: Int
+    let max_players: Int
+    let min_playtime: Int
+    let max_playtime: Int
+    let min_age: Int
+    let description: String
+    let description_preview: String
+    let thumb_url: String
+    let image_url: String
+    let url: String
+    let price: Float
+    let msrp: Float
+    let discount: Float
+    let primary_publisher: [String]
+//    let mechanics: String
+//    let categories: String
+    let designers: [String]
+    let developers: [String]
+    let artists: [String]
+    let reddit_all_time_count: Int
+    let reddit_week_count: Int
+    let reddit_day_count: Int
+}
+
+class Search {
+    var url: URL? //https://api.boardgameatlas.com/api/search?ids=TAAifFP590&client_id=JLBr5npPhV
+
+    func search() {
+        url = URL(string:"https://api.boardgameatlas.com/api/search?")
+        let query = URLQueryItem(name: "name", value: "Monopoly")
+        let clientID = URLQueryItem(name: "client_id", value: "GEFPATEZMn")
+        if #available(iOS 16.0, *) {
+            url?.append(queryItems: [query, clientID])
+        } else {
+            // Fallback on earlier versions
+            fatalError()
+        }
+        if let url = url {
+            let request = URLRequest(url: url)
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if let data = data {
+                    let gameDetails: [GameDetails] = try! JSONDecoder().decode([GameDetails].self, from: data)
+                    print(gameDetails)
+                }
+
+                if let response = response {
+                    print("RESPONSE \(response.description)")
+                }
+
+                if let error = error {
+                    print("ERROR \(error.localizedDescription)")
+                }
+            }
+            .resume()
+
+        }
+
+
+
     }
 }
--- Tests/BoardGameSearchTests/BoardGameSearchTests.swift
+++ Tests/BoardGameSearchTests/BoardGameSearchTests.swift
@@ -6,6 +6,7 @@ final class BoardGameSearchTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(BoardGameSearch().text, "Hello, World!")
+        Search().search()
+        
     }
 }
