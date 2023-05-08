1fa9bbfa897a834635396cc3fda4901ab86e56ca
Robert <55030468+robertb87@users.noreply.github.com>
2023-05-08 20:28:28 +0100

    coding keys

--- Sources/BoardGameSearch/BoardGameSearch.swift
+++ Sources/BoardGameSearch/BoardGameSearch.swift
@@ -11,38 +11,149 @@ public struct GameMechanics: Codable {
     let id: String
 }
 
-public struct SearchResults: Codable {
+public struct SearchResults: Decodable {
     let games: [GameDetails]
     let count: Int
 }
 
-public struct GameDetails: Codable {
+public struct GameDetails: Decodable {
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case names
+        case yearPublished      = "year_published"
+        case minPlayers         = "min_players"
+        case maxPlayers         = "max_players"
+        case minPlaytime        = "min_playtime"
+        case maxPlaytime        = "max_playtime"
+        case minAge             = "min_age"
+        case description
+        case descriptionPreview = "description_preview"
+        case tumbleURL          = "thumb_url"
+        case imageURL           = "image_url"
+        case url
+        case price
+        case msrp
+        case discount
+        case primagePublisher   = "primary_publisher"
+        case mechanics
+        case categories
+        case designers
+        case developers
+        case artists
+    }
+
     let id: String
     let name: String
-    let names: [String]?
-    let year_published: Int?
-    let min_players: Int?
-    let max_players: Int?
-    let min_playtime: Int?
-    let max_playtime: Int?
-    let min_age: Int?
-    let description: String?
-    let description_preview: String?
-    let thumb_url: String?
-    let image_url: String?
-    let url: String?
-    //let price: String
-    //let msrp: Float
-   //let discount: Float
-    //let primary_publisher: [String]
-    //let mechanics: [GameMechanics]
-    //let categories: [GameMechanics]
-   // let designers: [String]
-    //let developers: [String]
-  //  let artists: [String]
-    //let reddit_all_time_count: Int
-    //let reddit_week_count: Int
-    //let reddit_day_count: Int
+    let names: [String]
+    let yearPublished: Int
+    let minPlayers: Int
+    let maxPlayers: Int
+    let minPlaytime: Int
+    let maxPlaytime: Int
+    let minAge: Int
+    let description: String
+    let descriptionPreview: String
+    let thumbUrl: String
+    let imageUrl: String
+//    let url: String?
+//    let price: String
+//    let msrp: Float
+//    let discount: Float
+//    let primaryPublisher: [String]
+//    let mechanics: [GameMechanics]
+//    let categories: [GameMechanics]
+    let designers: [String]
+    let developers: [String]
+    let artists: [String]
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        id                          = try container.decode(String.self, forKey: .id)
+        name                        = try container.decode(String.self, forKey: .name)
+        if let names                = try? container.decode([String].self, forKey: .names) {
+            self.names = names
+        } else {
+            self.names = [String]()
+        }
+        if let yearPublished        = try? container.decode(Int.self, forKey: .yearPublished) {
+            self.yearPublished = yearPublished
+        } else  {
+            self.yearPublished = -1
+        }
+        if let minPlayers           = try? container.decode(Int.self, forKey: .minPlayers) {
+            self.minPlayers = minPlayers
+        } else {
+            self.minPlayers = -1
+        }
+        if let maxPlayers           = try? container.decode(Int.self, forKey:  .maxPlayers) {
+            self.maxPlayers = maxPlayers
+        } else {
+            self.maxPlayers = -1
+        }
+        if let minPlaytime          = try? container.decode(Int.self, forKey: .minPlaytime) {
+            self.minPlaytime = minPlaytime
+        } else {
+            self.minPlaytime = -1
+        }
+        if let maxPlaytime          = try? container.decode(Int.self, forKey: .maxPlaytime) {
+            self.maxPlaytime = maxPlaytime
+        } else {
+            self.maxPlaytime = -1
+        }
+        if let minAge               = try? container.decode(Int.self, forKey: .minAge) {
+            self.minAge = minAge
+        } else {
+            self.minAge = -1
+        }
+        if let description          = try? container.decode(String.self, forKey: .description) {
+            self.description = description
+        } else {
+            self.description = "No description"
+        }
+        if let descriptionPreview   = try? container.decode(String.self, forKey: .descriptionPreview) {
+            self.descriptionPreview = descriptionPreview
+        } else {
+            self.descriptionPreview = ""
+        }
+        if let thumbUrl             = try? container.decode(String.self, forKey: .tumbleURL) {
+            self.thumbUrl = thumbUrl
+        } else {
+            self.thumbUrl = ""
+        }
+        if let imageUrl             = try? container.decode(String.self, forKey: .imageURL) {
+            self.imageUrl = imageUrl
+        } else  {
+            self.imageUrl = ""
+        }
+//        price: String
+//        msrp: Float
+//        discount: Float
+//        primaryPublisher: [String]
+//        mechanics: [GameMechanics]
+//        categories: [GameMechanics]
+        if let designers            = try? container.decode([String].self, forKey: .designers) {
+            self.designers = designers
+        } else {
+            self.designers = []
+        }
+        if let developers           = try? container.decode([String].self, forKey: .developers) {
+            self.developers = developers
+        } else {
+            self.developers = []
+        }
+        if let artists              = try? container.decode([String].self, forKey: .artists) {
+            self.artists = artists
+        } else {
+            self.artists = []
+        }
+
+    }
+
+
+
 }
 
 public class Search {
